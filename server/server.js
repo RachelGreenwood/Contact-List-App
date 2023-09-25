9703839512851c902bde316ff15701f16d49217f
Rachel Greenwood <rachelgreenwood3301@gmail.com>
2023-09-24 22:16:51 -0500

    Sets up basic server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+// const db = require('./db/db-connection.js');
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+    res.json('Welcome to your contact list app!');
+});
+
+app.listen(PORT, () => console.log(`Hola! Server running on Port http://localhost:${PORT}`));
